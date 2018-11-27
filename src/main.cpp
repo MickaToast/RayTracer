d7d18154da9c738bb4b3a3fc5622ada6568f0e77
Mickaël Leclerc <mickael.leclerc@epitech.eu>
2018-11-27 11:27:38 +0000

    feat(travis): Added Travis CI file and launch gtest

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,25 @@
+/* Copyright (c) 2018 mickael.leclerc@epitech.eu charles.fraisse@epitech.eu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE. */
+
 #include <SFML/Graphics.hpp>
-#include "loader/OBJLoader.hh"
+#include "loader/OBJLoader.h"
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -11,14 +31,19 @@ int main(int argc, char **argv) {
 
     std::cout << "Loading scene " << argv[1] << "..." << std::endl;
     if (!loader.LoadFile(argv[1])) {
-        std::cerr << "Failed to load file. May have failed to find it or it was not an .obj file." << std::endl;
+        std::cerr << "Failed to load file. May have failed to "
+                     "find it or it was not an .obj file." << std::endl;
         return 1;
     }
 
-    std::cout << "DEBUG: Loaded " << loader.LoadedMeshes.size() << " meshes" << std::endl;
-    std::cout << "DEBUG: Loaded " << loader.LoadedVertices.size() << " vertices" << std::endl;
-    std::cout << "DEBUG: Loaded " << loader.LoadedIndices.size() << " indices" << std::endl;
-    std::cout << "DEBUG: Loaded " << loader.LoadedMaterials.size() << " materials" << std::endl;
+    std::cout << "DEBUG: Loaded " << loader.LoadedMeshes.size()
+            << " meshes" << std::endl;
+    std::cout << "DEBUG: Loaded " << loader.LoadedVertices.size()
+            << " vertices" << std::endl;
+    std::cout << "DEBUG: Loaded " << loader.LoadedIndices.size()
+            << " indices" << std::endl;
+    std::cout << "DEBUG: Loaded " << loader.LoadedMaterials.size()
+            << " materials" << std::endl;
 
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Work!");
     sf::CircleShape shape(100.f);
