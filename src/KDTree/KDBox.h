bace238c02a09026ff1cc45e0a986b8047d2116c
Charles FRAISSE <charles.fraisse@epitech.eu>
2018-12-03 15:04:42 +0000

    feat(KDBox): generating KDBox with min max set

---
+++ src/KDTree/KDBox.h
@@ -0,0 +1,41 @@
+/* Copyright (c) 2018 mickael.leclerc@epitech.eu charles.fraisse@epitech.eu
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE. */
+
+#pragma once
+
+#include <vector>
+#include "../Mesh/Triangle.h"
+
+namespace rt {
+class KDBox {
+ public:
+    KDBox(std::vector<Triangle> const& triangles);
+    ~KDBox();
+
+    Vector2<float> const& GetX() const;
+    Vector2<float> const& GetY() const;
+    Vector2<float> const& GetZ() const;
+
+ private:
+    Vector2<float>          _x;  // Containing Xmin and Xmax
+    Vector2<float>          _y;  // Containing Ymin and Ymax
+    Vector2<float>          _z;  // Containing Zmin and Zmax
+    std::vector<Triangle>   _triangles;
+
+    void    setMinMax(std::vector<Triangle> const& triangles);
+};
+}  // namespace rt
